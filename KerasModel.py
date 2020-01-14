3deae486342ed2c671a32fdd2f0fb76230a037cd
mmeyer <mareike.meyer@desy.de>
2020-01-14 16:04:07 +0100

    added possibility to run with balanced batches

--- KerasModel.py
+++ KerasModel.py
@@ -11,13 +11,17 @@ from keras.utils.np_utils import to_categorical
 from collections import deque
 import time
 import os
+from sklearn import model_selection
 
 class KerasObject():
 
-    def __init__(self, parameter_file = "", variables=[], target_names = {}, filename = "" ):
+    def __init__(self, parameter_file = "", variables=[], target_names = {}, filename = "", balancedbatches = "False", classes = [""], era = ''):
 
         self.variables = variables
         self.models = []
+        self.balancedbatches = balancedbatches
+        self.classes = classes
+        self.era = era
 
         try:
             if filename: self.load(filename)
@@ -85,27 +89,114 @@ class KerasObject():
 
     def trainSingle(self, train, test):
 
-
         # writing targets in keras readable shape
         best = str(int(time.time()))
-        y_train = to_categorical( train["target"].values )
         y_test  = to_categorical( test["target"].values )
 
-        N_classes = len(y_train[0])
+        N_classes = len(self.classes)
 
         model_impl = getattr(keras_models, self.params["name"]) # reads model defined in conf/keras_models.py
         model = model_impl(len(self.variables), N_classes)
         model.summary()
-        history = model.fit(
-            train[self.variables].values,
-            y_train,
-            sample_weight=train["train_weight"].values,
-            validation_split = 0.25,
-            # validation_data=(test[self.variables].values, y_test, test["train_weight"].values),
-            batch_size=self.params["batch_size"],
-            epochs=self.params["epochs"],
-            shuffle=True,
-            callbacks=[EarlyStopping(patience=self.params["early_stopping"]), ModelCheckpoint( best + ".model", save_best_only=True, verbose = 1) ])
+
+        if (self.balancedbatches=="True"):
+
+            print "---------------------------------Balanced batches are used --------------------------------"
+            x_train, x_val, y_train, y_val, w_train, w_val = model_selection.train_test_split(
+                                                             train[self.variables].values,
+                                                             train["target"].values,
+                                                             train["train_weight"].values,
+                                                             test_size=0.25,
+                                                             random_state=1234)
+
+            x_train_df = DataFrame(x_train, columns=train[self.variables].columns)
+            x_val_df = DataFrame(x_val, columns=train[self.variables].columns)
+
+            eras=[]
+            if self.era == 'Run2' :
+                eras=["era2016","era2017","era2018"]
+            else :
+                eras.append("era"+self.era)
+
+            eraIndexDict = {
+                era: {
+                    label: np.where((x_train_df[self.variables][era] == 1) & (y_train == i_class))[0]
+                    for i_class, label in enumerate(self.classes)
+                     }
+                for i_era, era in enumerate(eras)
+                }
+
+            def balancedBatchGenerator(eventsPerClassAndBatch):
+                    while True:
+                        nperClass = int(eventsPerClassAndBatch)
+                        selIdxDict = {
+                            era: {
+                                label: eraIndexDict[era][label][np.random.randint(
+                                    0, len(eraIndexDict[era][label]), nperClass)]
+                                for label in self.classes
+                            }
+                            for era in eras
+                        }
+                        y_collect = to_categorical(np.concatenate([
+                            y_train[selIdxDict[era][label]] for label in self.classes
+                            for era in eras
+                        ]))
+                        x_collect = np.concatenate([
+                            x_train[selIdxDict[era][label], :] for label in self.classes
+                            for era in eras
+                        ])
+                        w_collect = np.concatenate([
+                            w_train[selIdxDict[era][label]] * (eventsPerClassAndBatch*len(self.classes) / np.sum(w_train[selIdxDict[era][label]])) for label in self.classes
+                            for era in eras
+                        ])
+                        yield x_collect, y_collect, w_collect
+
+            testIndexDict = {
+                era: {
+                    label: np.where((x_val_df[self.variables][era] == 1) & (y_val == i_class))[0]
+                    for i_class, label in enumerate(self.classes)
+                    }
+                for i_era, era in enumerate(eras)
+            }
+
+            def calculateValidationWeights(x_val, y_val, w_val):
+                y_collect = to_categorical(np.concatenate([
+                    y_val[testIndexDict[era][label]] for label in self.classes
+                    for era in eras
+                    ]))
+                x_collect = np.concatenate([
+                    x_val[testIndexDict[era][label], :] for label in self.classes
+                    for era in eras
+                    ])
+                w_collect = np.concatenate([
+                    w_val[testIndexDict[era][class_]] * (len(x_val) / np.sum(w_val[testIndexDict[era][class_]]))
+                    for class_ in self.classes
+                    for era in eras
+                    ])
+                return x_collect, y_collect, w_collect
+
+            x_val, y_val, w_val = calculateValidationWeights(x_val, y_val, w_val)
+
+            history = model.fit_generator(
+                    balancedBatchGenerator(self.params["eventsPerClassAndBatch"]),
+                    steps_per_epoch=self.params["steps_per_epoch"]*len(eras),
+                    epochs=self.params["epochs"],
+                    callbacks=[EarlyStopping(patience=self.params["early_stopping"]), ModelCheckpoint( best + ".model", save_best_only=True, verbose = 1) ],
+                    validation_data=(x_val, y_val, w_val)
+            )
+        else :
+            print "---------------------------------Balanced batches are NOT used --------------------------------"
+            y_train = to_categorical( train["target"].values )
+            history = model.fit(
+                train[self.variables].values,
+                y_train,
+                sample_weight=train["train_weight"].values,
+                validation_split = 0.25,
+                # validation_data=(test[self.variables].values, y_test, test["train_weight"].values),
+                batch_size=self.params["batch_size"],
+                epochs=self.params["epochs"],
+                shuffle=True,
+                callbacks=[EarlyStopping(patience=self.params["early_stopping"]), ModelCheckpoint( best + ".model", save_best_only=True, verbose = 1) ])
 
         import matplotlib as mpl
         mpl.use('Agg')
@@ -151,4 +242,3 @@ class KerasObject():
 
         return DataFrame(dtype = float, data = {"predicted_class":prediction.idxmax(axis=1).values,
                                  "predicted_prob": prediction.max(axis=1).values } )
-
